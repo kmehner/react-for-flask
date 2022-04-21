89adcf06b10faba6ac9ddd1a8e491e9a3502b080
Katelyn Mehner <kmehner@gmail.com>
2022-04-20 20:29:10 -0600

    Fixed Login error

--- react-blog/src/views/Login.js
+++ react-blog/src/views/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { Buffer } from 'buffer';
 
 export default function Login(props){
     let navigate = useNavigate();
@@ -14,39 +15,44 @@ export default function Login(props){
         let username = e.target.username.value;
         let password = e.target.password.value;
 
-        let myHeaders = new Headers();
-        myHeaders.append('Authorization', "Basic " + btoa(`${username}:${password}`))
+        var myHeaders = new Headers();
+        // btoa wasn't working. I think because it is within the function and not class with async handleSubmit 
+        // Used Buffer and converted to string 
+        myHeaders.append('Authorization', "Basic " + Buffer.from(`${username}:${password}`).toString('base64'));
 
         var requestOptions = {
             method: 'POST',
             headers: myHeaders,
             redirect: 'follow'
+        };
 
-        }
+        fetch(`${base_url}auth/token`, requestOptions)
+            .then(res => {
+                if (res.status === 401 ){ 
+                    props.flashMessage("Username and/or password is incorrect.", "danger")
+                } else{
+                    res.json()
+                    .then (data => {
+                        if (data.error){
+                            this.props.flashMessage('Your username/password is incorrect', 'danger')
+                        } else {
+                            let token = data.token 
+                            localStorage.setItem('token', token)
+                            props.flashMessage("You have successfully logged in.", "success")
+                            props.login()
+                            navigate('/')
 
-        fetch(`${base_url}/auth/token`, requestOptions)
-            .then(res => res.json())
-            .then (data => {
-
-                if (data.error){
-                    this.props.flashMessage('Your username/password is incorrect', 'danger')
-                } else {
-                    let token = data.token 
-                    localStorage.setItem('token', token)
-                    props.flashMessage("You have successfully logged in.", "success")
-                    props.login()
-                    navigate('/')
-
+                        }
+                    })
                 }
-            })
-    } 
+            }
+    )}
 
     return(
         <form onSubmit={handleSubmit}>
             <h1>Log In</h1>
             <div className="row">
                 <div className="form-group">
-                <label htmlFor="email">Email</label>
 
                     <label htmlFor="username">Username</label>
                     <input type='text' name="username" className="form-control" placeholder="Username"></input>
@@ -54,12 +60,9 @@ export default function Login(props){
                     <label htmlFor="password">Password</label>
                     <input type='text' name="password" className="form-control" placeholder="Password"></input>
 
-                    <input type="submit" className="btn btn-dark mt-2 w-100" value="Sign Up"/>
+                    <input type="submit" className="btn btn-dark mt-2 w-100" value="Login"/>
                 </div>
             </div>
         </form>
     )
-
-
-
 }
